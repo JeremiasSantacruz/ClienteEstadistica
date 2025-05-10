b8d2cca19b3d6ce374dfaf965043858677366b56
Jeremias Santacruz <jeremias.santacruz@proveedor.ues21.edu.ar>
2025-05-10 10:46:12 -0300

    Git init

---
+++ src/test/java/com/example/estadsticacliente/EstadsticaClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.estadsticacliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstadsticaClienteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
