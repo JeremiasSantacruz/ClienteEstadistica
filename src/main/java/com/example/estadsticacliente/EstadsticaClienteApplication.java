b99739f4375abe222fe9c4e546f07f4df87ae7c5
Jeremias Santacruz <jeremias.santacruz@proveedor.ues21.edu.ar>
2025-05-10 10:46:12 -0300

    Git init

---
+++ src/main/java/com/example/estadsticacliente/EstadsticaClienteApplication.java
@@ -0,0 +1,13 @@
+package com.example.estadsticacliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstadsticaClienteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstadsticaClienteApplication.class, args);
+    }
+
+}
