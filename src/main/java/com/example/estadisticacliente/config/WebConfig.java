2b111ca391e3e8e9a2151ea908d6c79b74568fb0
Jeremias Santacruz <jeremias.santacruz@proveedor.ues21.edu.ar>
2025-05-11 17:19:10 -0300

    Add cors

---
+++ src/main/java/com/example/estadisticacliente/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.example.estadisticacliente.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Ajusta la ruta según necesites
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos permitidos
+                .allowedHeaders("*") // Cabeceras permitidas
+                .allowCredentials(true); // Si necesitas enviar cookies o autenticación
+    }
+}
